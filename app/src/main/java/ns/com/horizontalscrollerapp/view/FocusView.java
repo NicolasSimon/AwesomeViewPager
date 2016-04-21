ab9766f1b50f159368529ba28136eb3bd218bec1
Nicolas Simon <nicolas@touchnote.com>
2016-04-21 10:06:29 +0100

    draggable focusView

--- app/src/main/java/ns/com/horizontalscrollerapp/view/FocusView.java
+++ app/src/main/java/ns/com/horizontalscrollerapp/view/FocusView.java
@@ -5,17 +5,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by Nicolas on 21/04/2016.
  * (c) Touchnote Ltd., 2015
  */
-public class FocusView extends View {
+public class FocusView extends View implements View.OnTouchListener {
     private Paint               mTransparentPaint;
     private Paint               mSemiBlackPaint;
     private Path                mPath = new Path();
+
+    private Point               mTransparentPosition;
 
     public FocusView(Context context) {
         super(context);
@@ -40,19 +44,35 @@ public class FocusView extends View {
         mSemiBlackPaint = new Paint();
         mSemiBlackPaint.setColor(Color.BLACK);
         mTransparentPaint.setStrokeWidth(10);
+        setOnTouchListener(this);
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN :
+            case MotionEvent.ACTION_MOVE:
+                mTransparentPosition.set((int)event.getRawX(), (int)event.getRawY());
+                invalidate();
+                return (true);
+        }
+        return false;
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        if (mTransparentPosition == null) {
+            mTransparentPosition = new Point(canvas.getWidth() / 2, canvas.getHeight() / 2);
+        }
+
         mPath.reset();
 
-        mPath.addCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, canvas.getWidth() / 2, Path.Direction.CW);
+        mPath.addCircle(mTransparentPosition.x, mTransparentPosition.y, canvas.getWidth() / 2, Path.Direction.CW);
         mPath.setFillType(Path.FillType.INVERSE_EVEN_ODD);
 
-        canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, canvas.getWidth() / 2, mTransparentPaint);
+        canvas.drawCircle(mTransparentPosition.x, mTransparentPosition.y, canvas.getWidth() / 2, mTransparentPaint);
 
         canvas.drawPath(mPath, mSemiBlackPaint);
         canvas.clipPath(mPath);
